55b90f75458b9c12083df9d86c9efe869d8f4b59
Javier Alejandro Puras Lombillo <javijandro99@gmail.com>
2024-03-02 12:56:40 +0100

    Paso 5.6

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.unscramble.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,6 +15,8 @@ class GameViewModel: ViewModel()
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
     private lateinit var currentWord: String
     private var usedWords: MutableSet<String> = mutableSetOf()
+    var userGuess by mutableStateOf("")
+        private set
 
     init
     {
@@ -46,5 +51,10 @@ class GameViewModel: ViewModel()
     {
         usedWords.clear()
         _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+
+    fun updateUserGuess(guessedWord: String)
+    {
+        userGuess = guessedWord
     }
 }
